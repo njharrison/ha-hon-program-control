1b73b3143a48832874ee17288a558fb04b94382d
njharrison <njharrison@gmail.com>
2026-09-21 00:24:34 +0100

    Add custom_components/hon_program_control/select.py

---
+++ custom_components/hon_program_control/select.py
@@ -0,0 +1,104 @@
+from __future__ import annotations
+
+from homeassistant.components.select import SelectEntity
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+from .const import DOMAIN
+
+
+PARAMETERS = {
+    "temp": ("Temperature", "mdi:thermometer"),
+    "spinSpeed": ("Spin Speed", "mdi:rotate-right"),
+    "dirtyLevel": ("Soil Level", "mdi:brightness-6"),
+}
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    controller = hass.data[DOMAIN][entry.entry_id]
+    entities = [ProgramSelect(controller)]
+    entities.extend(
+        ParameterSelect(controller, key, name, icon)
+        for key, (name, icon) in PARAMETERS.items()
+    )
+    async_add_entities(entities)
+
+
+class BaseProgramSelect(SelectEntity):
+    _attr_has_entity_name = True
+
+    def __init__(self, controller):
+        self.controller = controller
+        self._remove_listener = None
+
+    @property
+    def device_info(self):
+        return self.controller.device.device_info
+
+    async def async_added_to_hass(self):
+        await super().async_added_to_hass()
+        self._remove_listener = self.controller.add_listener(
+            self.async_write_ha_state
+        )
+
+    async def async_will_remove_from_hass(self):
+        if self._remove_listener:
+            self._remove_listener()
+        await super().async_will_remove_from_hass()
+
+
+class ProgramSelect(BaseProgramSelect):
+    _attr_name = "Program"
+    _attr_icon = "mdi:washing-machine"
+
+    def __init__(self, controller):
+        super().__init__(controller)
+        self._attr_unique_id = f"{controller.entry.data['mac']}_program_control_program"
+
+    @property
+    def options(self):
+        return self.controller.programs
+
+    @property
+    def current_option(self):
+        return self.controller.program
+
+    async def async_select_option(self, option: str) -> None:
+        self.controller.select_program(option)
+
+
+class ParameterSelect(BaseProgramSelect):
+    def __init__(self, controller, key, name, icon):
+        super().__init__(controller)
+        self.key = key
+        self._attr_name = name
+        self._attr_icon = icon
+        self._attr_unique_id = (
+            f"{controller.entry.data['mac']}_program_control_{key}"
+        )
+
+    @property
+    def available(self):
+        return bool(self.controller.parameter_options(self.key))
+
+    @property
+    def options(self):
+        options = self.controller.parameter_options(self.key)
+        return options or ["Unavailable"]
+
+    @property
+    def current_option(self):
+        value = self.controller.parameter_value(self.key)
+        if value is None:
+            return "Unavailable"
+        return value
+
+    async def async_select_option(self, option: str) -> None:
+        if option == "Unavailable":
+            return
+        self.controller.set_parameter(self.key, option)
