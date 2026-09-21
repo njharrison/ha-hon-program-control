d0420b1e5665c220ecd9fd513d3bd38f61505d63
njharrison <njharrison@gmail.com>
2026-09-21 19:44:30 +0100

    Add save preset button

--- custom_components/hon_program_control/button.py
+++ custom_components/hon_program_control/button.py
@@ -14,23 +14,46 @@ async def async_setup_entry(
     async_add_entities: AddEntitiesCallback,
 ) -> None:
     controller = hass.data[DOMAIN][entry.entry_id]
-    async_add_entities([StartProgramButton(controller)])
+    async_add_entities([
+        SavePresetButton(controller),
+        StartProgramButton(controller),
+    ])
 
 
-class StartProgramButton(ButtonEntity):
+class BaseProgramButton(ButtonEntity):
     _attr_has_entity_name = True
-    _attr_name = "Start selected program"
-    _attr_icon = "mdi:play"
 
     def __init__(self, controller):
         self.controller = controller
-        self._attr_unique_id = (
-            f"{controller.entry.data['mac']}_program_control_start"
-        )
 
     @property
     def device_info(self):
         return self.controller.device.device_info
+
+
+class SavePresetButton(BaseProgramButton):
+    _attr_name = "Save to preset"
+    _attr_icon = "mdi:content-save"
+
+    def __init__(self, controller):
+        super().__init__(controller)
+        self._attr_unique_id = (
+            f"{controller.entry.data['mac']}_program_control_save_preset"
+        )
+
+    async def async_press(self) -> None:
+        await self.controller.async_save_preset()
+
+
+class StartProgramButton(BaseProgramButton):
+    _attr_name = "Start selected program"
+    _attr_icon = "mdi:play"
+
+    def __init__(self, controller):
+        super().__init__(controller)
+        self._attr_unique_id = (
+            f"{controller.entry.data['mac']}_program_control_start"
+        )
 
     async def async_press(self) -> None:
         await self.controller.async_start()
